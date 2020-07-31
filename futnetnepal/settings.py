681f2fccd27390b0896ea9da97aa278d7978e003
Shekhar Dhakal <33958757+proxydhakal@users.noreply.github.com>
2020-07-31 22:48:26 +0545

    Update settings.py

--- futnetnepal/settings.py
+++ futnetnepal/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ur^khr2u!kph7dxrgz((@of_i1_=td!2)wgnw$91q@e=oa$te*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['139.59.146.183']
 
 
 # Application definition
