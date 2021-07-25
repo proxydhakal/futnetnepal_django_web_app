c202b344f43f361752728c94e983f08893100215
Shekhar Dhakal <shekhardhakal2015@gmail.com>
2021-07-25 23:39:00 +0545

    email verification and password reset added

--- futnetnepal/settings.py
+++ futnetnepal/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ur^khr2u!kph7dxrgz((@of_i1_=td!2)wgnw$91q@e=oa$te*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*","futnetnepal.tk","www.futnetnepal.tk","143.244.140.109"]
 
 
 # Application definition
